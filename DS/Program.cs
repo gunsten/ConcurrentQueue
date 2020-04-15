1111493ffe3c36dcdc7b836546eb45336638620c
gunsten <gunnar.stenlund@gmail.com>
2020-04-15 22:07:24 +0200

    ConcurrentQueue impl and tests

---
+++ DS/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Threading;
+using Queue;
+
+namespace DS
+{
+    class Consumer
+    {
+        public Consumer(ConcurrentQueue<int> queue)
+        {
+            this.queue = queue;
+        }
+        public void Sum()
+        {
+            while(run)
+            {
+                try
+                {
+                    sum += queue.deQueue();
+                } catch(EmptyQueueException)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            run = false;
+        }
+
+        public int GetResult()
+        {
+            return sum;
+        }
+
+        private int sum = 0;
+        private bool run = true;
+        private ConcurrentQueue<int> queue;
+    }
+
+    class Producer
+    {
+        public Producer(ConcurrentQueue<int> queue, int amount, int intSize)
+        {
+            this.queue = queue;
+            this.amount = amount;
+            this.intSize = intSize;
+            list = new int[amount];
+        }
+        public void Produce()
+        {
+            var rand = new Random();
+            for (int i = 0; i < amount; i++)
+            {
+                list[i] = rand.Next() % intSize;
+                queue.enQueue(list[i]);
+            }
+        }
+        public int Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < amount; i++)
+            {
+                sum += list[i];
+            }
+            return sum;
+        }
+        private ConcurrentQueue<int> queue;
+        private int[] list;
+        private int amount, intSize;
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //testQueue();
+            //testLNode();
+
+            ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+
+            const int THREADS = 4;
+
+            Thread[] consumerts = new Thread[THREADS];
+            Consumer[] consumers = new Consumer[THREADS];
+            Producer producer = new Producer(queue, 100000, 10);
+            Thread producert = new Thread(new ThreadStart(producer.Produce));
+
+            for(int i=0;i<THREADS;i++)
+            {
+                Consumer temp = new Consumer(queue);
+                consumers[i] = temp;
+                consumerts[i] = new Thread(new ThreadStart(temp.Sum));
+            }
+
+            producert.Start();
+
+            for (int i = 0; i < THREADS; i++)
+            {
+                consumerts[i].Start();
+            }
+
+            producert.Join(); // wait for producer to finish
+
+            //Busy-wait, TODO: solve with call-back
+            while(!queue.isEmpty())
+            {
+                Thread.Sleep(100);
+            }
+
+            // queue empty, stop threads
+            for (int i = 0; i < THREADS; i++)
+            {
+                consumers[i].Stop();
+                consumerts[i].Join(); // Wait for consumer to finish
+            }
+
+            Console.WriteLine("Producer sum: " + producer.Sum());
+
+            int sum = 0;
+            for (int i = 0; i < THREADS; i++)
+            {
+                sum += consumers[i].GetResult();
+            }
+            Console.WriteLine("Consumers sum: " + sum);
+        }
+
+        public static void testLNode()
+        {
+            LNode<int> node = new LNode<int>(null, 4);
+            node.acquire();
+            Console.WriteLine("LNode, expect: 4, result: " + node.getContent());
+            node.release();
+            bool exc = false;
+            try
+            {
+                Console.WriteLine("LNode, should crash: " + node.getContent());
+            } catch(ConcurrentQueueException)
+            {
+                exc = true;
+            }
+            Console.WriteLine("LNode locked: " + exc);
+        }
+
+        public static void testQueue()
+        {
+            Queue<int> queue = new Queue<int>();
+            Console.WriteLine("isEmpty, expects: True, result: " + queue.isEmpty());
+            queue.enQueue(1);
+            Console.WriteLine("Content, expects: 1, result: " + queue.peek());
+            queue.deQueue();
+            Console.WriteLine("deQueued, expects: True, result: " + queue.isEmpty());
+            Console.WriteLine("Order, expects: True, result: " + testQueueRandom(50, 5));
+            bool exc = false;
+            try
+            {
+                queue.deQueue();
+            }
+            catch (EmptyQueueException)
+            {
+                exc = true;
+            }
+            Console.WriteLine("Exc on deQueue, expects: True, result: " + exc);
+            exc = false;
+            try
+            {
+                queue.peek();
+            }
+            catch (EmptyQueueException)
+            {
+                exc = true;
+            }
+            Console.WriteLine("Exc on peek, expects: True, result: " + exc);
+        }
+
+        public static bool testQueueRandom(int listSize, int intSize)
+        {
+            var rand = new Random();
+            int[] list = new int[listSize];
+            Queue<int> queue = new Queue<int>();
+            for(int i=0;i<listSize;i++) 
+            {
+                list[i] = rand.Next() % intSize;
+                queue.enQueue(list[i]);
+            }
+            // Test order
+            for(int i=0;i<listSize;i++)
+            {
+                if(queue.deQueue() != list[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Queue/Queue.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Threading;
+
+namespace Queue
+{
+    public class Queue<T>
+    {
+        public Queue()
+        {
+            start = end = null;
+        }
+
+        public void enQueue(T content)
+        {
+            Node<T> temp = new Node<T>(content);
+
+            if(isEmpty())
+            {
+                start = end = temp;
+                return;
+            }
+
+            end.setNext(temp); // Add temp node last
+            end = temp; // Change pointer
+        }
+
+        public T deQueue()
+        {
+            if (isEmpty())
+            {
+                throw new EmptyQueueException("Queue is empty");
+            }
+            Node<T> temp = start; // Store node to dequeue
+            start = start.getNext(); // Move pointer
+
+            // If start becomes null, queue is empty
+            if(start == null) { end = null; }
+
+            return temp.getContent();
+        }
+
+        public T peek()
+        {
+            if (isEmpty())
+            {
+                throw new EmptyQueueException("Queue is empty");
+            }
+            return start.getContent();
+        }
+
+        public bool isEmpty()
+        {
+            return end == null;
+        }
+
+        private Node<T> start, end;
+    }
+
+    public class ConcurrentQueue<T>
+    {
+        public ConcurrentQueue()
+        {
+            start = end = null;
+        }
+
+        public void enQueue(T content)
+        {
+            endPointerMutex.WaitOne();
+            Node<T> temp = new Node<T>(content);
+
+            if(end == null) // Queue is empty, dequeue is impossible and queue is effectively locked
+            {               // Thus safe to modify start
+                start = end = temp;
+                endPointerMutex.ReleaseMutex();
+                return;
+            }
+
+            end.setNext(temp); // Add temp node last
+            end = temp; // Change pointer
+
+            endPointerMutex.ReleaseMutex();
+        }
+
+        public T deQueue()
+        {
+            startPointerMutex.WaitOne();
+            if (start == null)
+            {
+                startPointerMutex.ReleaseMutex();
+                throw new EmptyQueueException("Queue is empty");
+            }
+
+            T temp = start.getContent(); // Store node to dequeue
+
+            start = start.getNext(); // Move pointer
+
+            // If start becomes null, queue is empty
+            if(start == null) {
+                endPointerMutex.WaitOne();
+                end = null;
+                endPointerMutex.ReleaseMutex();
+            }
+
+            startPointerMutex.ReleaseMutex();
+
+            return temp;
+        }
+
+        public T peek()
+        {
+            startPointerMutex.WaitOne();
+            if (start == null)
+            {
+                startPointerMutex.ReleaseMutex();
+                throw new EmptyQueueException("Queue is empty");
+            }
+            T content = start.getContent();
+            startPointerMutex.ReleaseMutex();
+            return content;
+        }
+
+        // TODO: probably not needed if call-backs implemented
+        public bool isEmpty()
+        {
+            endPointerMutex.WaitOne();
+            bool isempty = end == null;
+            endPointerMutex.ReleaseMutex();
+            return isempty;
+        }
+
+        private Node<T> start, end;
+        private Mutex startPointerMutex = new Mutex(), endPointerMutex = new Mutex();
+    }
+
+    public class Node<T>
+    {
+        public Node()
+        {
+            next = null;
+            content = default(T);
+        }
+
+        public Node(T content)
+        {
+            next = null;
+            this.content = content;
+        }
+
+        public Node(Node<T> next, T content)
+        {
+            this.next = next;
+            this.content = content;
+        }
+
+        public Node<T> getNext()
+        {
+            return next;
+        }
+
+        public T getContent()
+        {
+            return content;
+        }
+
+        public void setContent(T content)
+        {
+            this.content = content;
+        }
+
+        public void setNext(Node<T> next)
+        {
+            this.next = next;
+        }
+
+        private Node<T> next;
+        private T content;
+    }
+
+    public class LNode<T> : Node<T>
+    {
+        public LNode() : base() { }
+
+        public LNode(T content) : base(content) { }
+
+        public LNode(LNode<T> next, T content) : base(next, content) { }
+
+        public void acquire()
+        {
+            mutex.WaitOne();
+            owner = Thread.CurrentThread;
+        }
+
+        public void release()
+        {
+            owner = null;
+            mutex.ReleaseMutex();
+        }
+
+        public new LNode<T> getNext()
+        {
+            if(!Thread.CurrentThread.Equals(owner))
+            {
+                throw new ConcurrentQueueException("getNext: Current thread is not owner");
+            }
+            return (LNode<T>) base.getNext();
+        }
+
+        public new T getContent()
+        {
+            if (!Thread.CurrentThread.Equals(owner))
+            {
+                throw new ConcurrentQueueException("getContent: Current thread is not owner");
+            }
+            return base.getContent();
+        }
+
+        public new void setContent(T content)
+        {
+            if (!Thread.CurrentThread.Equals(owner))
+            {
+                throw new ConcurrentQueueException("setContent: Current thread is not owner");
+            }
+            base.setContent(content);
+        }
+
+        public new void setNext(Node<T> next)
+        {
+            if (!Thread.CurrentThread.Equals(owner))
+            {
+                throw new ConcurrentQueueException("setNext: Current thread is not owner");
+            }
+            base.setNext(next);
+        }
+
+        ~LNode()
+        {
+            mutex.Dispose();
+        }
+
+        Mutex mutex = new Mutex();
+        Thread owner = null;
+    }
+
+    public class EmptyQueueException : Exception
+    {
+        public EmptyQueueException(string message) : base(message) { }
+    }
+
+    public class ConcurrentQueueException : Exception
+    {
+        public ConcurrentQueueException(string message) : base(message) { }
+    }
+}
